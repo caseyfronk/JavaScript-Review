2ac5594b8d958fe63c1baccc2961c927aa2f4b09
Casey <caseyfronk@hotmail.com>
2017-05-20 12:07:14 -0600

    Finished objectReview

--- objectReview.js
+++ objectReview.js
@@ -1,20 +1,29 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+var favoriteThings = {
+    band: "Of Mice & Men",
+    food: "Sushi",
+    person: "Kellin",
+    book: "The Phoenix Project",
+    movie: "The Wolf of Wallstreet",
+    holiday: "Christmas"
+}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+favoriteThings.car = "BMW M3";
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+favoriteThings.food = "Lettuce";
+
+favoriteThings.book = "50 Shades of Gray";
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+alert(favoriteThings.person);
 
-
+alert(favoriteThings.book);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -31,16 +40,20 @@ var user = {
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+for(var key in user) {
+    if(!user[key]) {
+        delete user[key];
+    }
+}
 
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+user.name = 'Casey Fronk';
+user.username = 'caseyfronk';
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
-
+console.log(user);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -49,17 +62,24 @@ var user = {
 
 //create an empty object called methodCollection. 
 
-  //Your code here
+var methodCollection = {
+    
+}
 
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
-  //Your code here
+methodCollection.alertHello = function() {
+    alert('hello');
+}
+
+methodCollection.logHello = function(msg) {
+    console.log(msg);
+}
 
 //Now call your alertHello and logHello methods. 
 
-  //Your code here
-
-
+methodCollection.alertHello();
+methodCollection.log('hello');
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -68,5 +88,33 @@ var user = {
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
 
+function voweler(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    var result = {
+        a: 0,
+        e: 0,
+        i: 0,
+        o: 0,
+        u: 0,
+        y: 0
+    };
+    for(var i = 0; i < str.length; i++) {
+        for(var j = 0; j < vowels.length; j++) {
+            if(str[i] === vowels[j]) {
+                if(!result.hasOwnProperty(vowels[j])) {
+                   result[vowels[j]] = 1;
+                   } else {
+                       result[vowels[j]]++;
+                   }
+            }
+        }
+    }
+    for(var key in result) {
+        if(!result[key]) {
+            delete result[key];
+        }
+    }
+    return result;
+}
 
-
+voweler("This is a test");
